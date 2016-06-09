4eb7e8a5672f44c979e1dad35fbdd62b774c3f81
lihiv <lihiv92@gmail.com>
2016-06-09 16:32:30 +0300

    new project

--- app/src/main/java/com/example/user/mini_project/MainActivity.java
+++ app/src/main/java/com/example/user/mini_project/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//hiii gal!
+    //diudiciducsni
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void onCreate2(Bundle savedInstanceState) { //TODO delete!!
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
 }
